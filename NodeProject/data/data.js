5c82e093c7fafaa9c7a2f99a8a27b59227bc83ba
Radhika Mittal <158038107+radhika5903@users.noreply.github.com>
2024-11-15 15:43:32 +0530

    task

---
+++ NodeProject/data/data.js
@@ -0,0 +1,15 @@
+const StudentsDetail = [
+    {
+    name:"Rahul",
+    rollno:45,
+    marks:56,
+    course:"Btech"
+    },
+    {
+        name:"Radhika",
+        rollno:51,
+        marks:56,
+        course:"Btech"
+    }
+]
+module.exports = StudentsDetail //used for requiring the file
